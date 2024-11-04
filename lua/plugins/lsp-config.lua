6fc1eb86f9a32f699ce12a863d07a1048c5c3895
Oliver.Lock <oliverlock@ntlworld.com>
2024-11-04 21:17:22 +0000

    Updated to add telescope theme for auto code completeion. Also added in none-ls which is com fork of null-ls for cli tools as language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,26 +1,29 @@
 -- List of tables as each table own extension
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require('lspconfig')
-            -- Setup Lua LSP
-            lspconfig.lua_ls.setup({})
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-        end
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			-- Setup Lua LSP
+			lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
