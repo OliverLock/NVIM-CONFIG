ef1ad4ec81b0f13a593c2f01dc87022fca088e19
Oliver.Lock <oliverlock@ntlworld.com>
2024-11-06 20:47:31 +0000

    Added none-ls and support for auto install of languages in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 
--- Setup Treesitter config
-    config = function()
-    local configs = require("nvim-treesitter.configs")
-        configs.setup({
-         ensure_installed = {"lua", "javascript"},
-         highlight = { enable = true },
-         indent = { enable = true },
-         disable = { "markdown"}
-    })
-    end
+	-- Setup Treesitter config
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			disable = { "markdown" },
+		})
+	end,
 }
