f30f776a318e94decb866bf94ec7fd01a75acb70
Oliver Lock <oliverlock@ntlworld.com>
2025-01-02 16:19:26 +0000

    updated lua line length

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,24 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'dracula'
-            }
-
-        })
-   end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_b = {
+					{
+            "branch",
+            fmt = function(branch_name)
+              local max_length = 40
+              if #branch_name > max_length then
+                return branch_name:sub(1, max_length) .. "..."
+              end
+            return branch_name
+            end,
+          },
+				},
+			},
+		})
+	end,
 }
