723e4911dc4fdfcd3589c84222bd72418eba6002
Oliver Lock <oliverlock@ntlworld.com>
2025-01-04 22:18:29 +0000

    Added undo tree and made undo history persistant to file storage

--- lua/plugins/undo-tree.lua
+++ lua/plugins/undo-tree.lua
@@ -1,6 +1,11 @@
 return {
   "mbbill/undotree",
 
+  config = function()
+    vim.opt.undofile = true
+    vim.opt.undodir = vim.fn.expand("~/.config/nvim/undodir")
+  end,
+
   -- Keybindings
-  vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+  vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle),
 }
