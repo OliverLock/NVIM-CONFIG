c30d1cf30c4af38747fbbb7e058bca9b73397f62
Oliver Lock <oliverlock@ntlworld.com>
2025-01-04 11:23:22 +0000

    added markdown preview functionality auto opens

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,9 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle" },
+  ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
+  vim.keymap.set('n', '<leader>mp', "<cmd>MarkdownPreviewToggle<cr>")
+} -- Markdown Preview
