28e6544a12cd89d5b71d5e6bd9eca62f849962e2
Oliver Lock <oliverlock@ntlworld.com>
2025-01-05 17:50:04 +0000

    messed with some colours but commented out for per machine stuff

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,23 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, 
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
 
--- Setup catppuccin and set as theme
-    config = function()
-        local config = require("catppuccin")
-        config.setup({
-            flavour = "mocha"
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end,
+	-- Setup catppuccin and set as theme
+	config = function()
+		local config = require("catppuccin")
+		config.setup({
+			flavour = "mocha",
+			styles = {
+				comments = { "italic" },
+			},
+			custom_highlights = function()
+				return {
+				 --["@variable"] = { fg = "#e06c75" }, -- Change to a preferred color
+         -- ["@method"] = { fg = "#61afef"},
+				}
+			end,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -57,6 +57,7 @@ return {
       })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>r", vim.diagnostic.open_float, {})
       vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
