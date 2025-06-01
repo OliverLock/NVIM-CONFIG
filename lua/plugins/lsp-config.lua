2f5a249757d8e4358e39606abbb9f715369af4e5
Oliver Lock <oliverlock@ntlworld.com>
2025-06-01 14:57:27 +0100

    add global deffinition for vim not best soloution needs more work

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -47,6 +47,26 @@ return {
       -- Setup Lua LSP
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT for Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false, -- Don't prompt to configure third party
+            },
+            telemetry = {
+              enable = false, -- No telemetry
+            },
+          },
+        },
       })
       lspconfig.ts_ls.setup({
         capabilities = capabilities,
