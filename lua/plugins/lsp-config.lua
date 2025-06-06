0c7552cec10ab408644801485a000109dc09e3db
Oliver Lock <oliverlock@ntlworld.com>
2025-06-06 16:35:16 +0100

    Have removed Lua_ls manual setup and replaced with lazy-dev

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -43,30 +43,6 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      -- Setup Lua LSP
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT for Neovim)
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = { "vim" },
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false, -- Don't prompt to configure third party
-            },
-            telemetry = {
-              enable = false, -- No telemetry
-            },
-          },
-        },
-      })
       lspconfig.ts_ls.setup({
         capabilities = capabilities,
       })
