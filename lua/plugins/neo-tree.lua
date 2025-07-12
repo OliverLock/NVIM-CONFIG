e4b94ded615b286b0d2d3e4c51269787a8161a8f
Oliver Lock <oliverlock@ntlworld.com>
2025-07-12 12:09:13 +0100

    Added copy file path to neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,65 @@
 return {
-
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim",},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
 
     config = function()
+        -- Keymap to toggle Neo-tree
         vim.keymap.set('n', '<leader>e', ':Neotree filesystem toggle left<cr>', {})
-    end
 
+        -- Copy File Path Function
+        local function copy_path(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+                filepath,
+                modify(filepath, ":."),
+                modify(filepath, ":~"),
+                filename,
+                modify(filename, ":r"),
+                modify(filename, ":e"),
+            }
+
+            vim.ui.select({
+                "1. Absolute path: " .. results[1],
+                "2. Path relative to CWD: " .. results[2],
+                "3. Path relative to HOME: " .. results[3],
+                "4. Filename: " .. results[4],
+                "5. Filename without extension: " .. results[5],
+                "6. Extension of the filename: " .. results[6],
+            }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+                if choice then
+                    local i = tonumber(choice:sub(1, 1))
+                    if i then
+                        local result = results[i]
+                        vim.fn.setreg('"', result)
+                        vim.notify("Copied: " .. result)
+                    else
+                        vim.notify("Invalid selection")
+                    end
+                else
+                    vim.notify("Selection cancelled")
+                end
+            end)
+        end
+
+        -- Set up Neo-tree with custom cmd
+        require("neo-tree").setup({
+            filesystem = {
+                window = {
+                    mappings = {
+                        ["Y"] = copy_path,
+                    },
+                },
+            },
+        })
+    end,
 }
+
