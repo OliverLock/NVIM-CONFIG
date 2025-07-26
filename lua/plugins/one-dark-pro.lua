cb0669e109e305f71f94188cdf09827b0be780a6
Oliver Lock <oliverlock@ntlworld.com>
2025-07-26 01:54:33 +0100

    started updating colour scheme of one dark pro further

--- lua/plugins/one-dark-pro.lua
+++ lua/plugins/one-dark-pro.lua
@@ -7,7 +7,18 @@ return {
 				onedark = { bg = "#23272e" }, -- one pro dark background
 			},
       highlights = {
+        -- Top Level Functions
+            ["@boolean"] = {fg = "#d19a66", style = "bold" },
+    --     ["@function.call"] = { style = "bold", bold = true},
+				-- ["@function.builtin"] = { style = "bold", bold = true },
+        -- Python Specific
+          --Python Standard 
         ["@odp.import_module.python"] = {fg = "#e5c07b", italic = true},
+          -- python function
+				["@function.method.call.python"] = {fg = "#61afef", bold = true  },
+      },
+      styles = {
+        functions = "italic,bold",
       },
 			options = {
 				transparency = true,
